0bb443948b0533cb00a959b7d7c6823ef8fcaf57
EmmaculateOwaga <emmahachieng2015@gmail.com>
2022-09-08 12:59:01 -0700

    Task 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,43 +1,22 @@
-#include <stdio.h>                                                               
-
-/**                                                                              
-
+#include <stdio.h>
+/**
 * main - print the string in the put function
-
-*                                  
-
-* Description: using the main function                                           
-
-* this program prints "programming is like building a multilingual puzzle"       
-
-* Return: 0                                                                      
-
-*/                                                                               
-
-int main(void)                                                                   
-
-{                                                                                
-
-char c;                                                                          
-
-int i;                                                                           
-
-long li;                                                                         
-
-long long lli;                                                                   
-
-float f;                                                                         
-
-printf("size of a char: %ld byte(s)\n", sizeof(c));                              
-
-printf("size of an int: %ld byte(s)\n", size of(i));                             
-
-printf("size of a long int: %ld byte(s)\n", size of(li));                        
-
-printf("size of a long long int: %ld byte(s)\n", size of(lli));                  
-
-printf("size of a float: %ld byte(s)\n", size of(f));                            
-
-return (0);                                                                      
-
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
+printf("size of a char: %ld byte(s)\n", sizeof(c));
+printf("size of an int: %ld byte(s)\n", size of(i));
+printf("size of a long int: %ld byte(s)\n", size of(li));
+printf("size of a long long int: %ld byte(s)\n", size of(lli));
+printf("size of a float: %ld byte(s)\n", size of(f));
+return (0);
 }
