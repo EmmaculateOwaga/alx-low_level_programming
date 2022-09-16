4b91c1a6e3bd063561e3a35331372ca3948fcc7d
EmmaculateOwaga <emmahachieng2015@gmail.com>
2022-09-16 12:40:28 -0700

    Task 10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,32 +9,27 @@
 
 void print_triangle(int size)
 {
-	int inc1, inc2;
-
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (inc1 = 1; inc1 <= size; inc1++)
-		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
-			{
-				putchar(' ');
-			}
-
-			for (inc2 = 0; inc2 < inc1; inc2++)
-			{
-				putchar('#');
-			}
-
-			if (inc1 == size)
-			{
-				continue;
-			}
-			
-			putchar('\n');
-		}
+		_putchar('\n');
 	}
 	else
 	{
-		putchar('\n');
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+			_putchar(' ');
+			}
+
+		for (j = 1; j <= i; j++)
+		{
+		_putchar('#');
+		}
+
+		_putchar('\n');
+		}
 	}
 }
