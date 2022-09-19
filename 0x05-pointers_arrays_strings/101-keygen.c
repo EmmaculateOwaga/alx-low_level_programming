db789ee20a97665fde64bd6542992f0fa3fc4397
EmmaculateOwaga <emmahachieng2015@gmail.com>
2022-09-19 11:47:54 -0700

    Task 11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,13 +14,13 @@ int main(void)
 
 	srand(time(NULL));
 	sum = 0;
-	while (sum <= 2645)
+	while (sum <= 1997)
 	{
 		pass = (rand() % 128);
 		sum += pass;
 		printf("%c", pass);
 	}
-	printf("%c", 2772 - sum);
+	printf("%c", 2124 - sum);
 
 	return (0);
 }
