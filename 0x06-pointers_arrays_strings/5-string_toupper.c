ea43cabbde1e6b570f525af4b18243bf28b871df
EmmaculateOwaga <emmahachieng2015@gmail.com>
2022-09-21 09:28:12 -0700

    Task 5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @y: string parameter
+ * Return: string
+ */
+
+char *string_toupper(char *y)
+{
+	int i = 0;
+
+	while (y[i])
+	{
+
+		if (y[i] >= 97 && y[i] <= 122)
+			y[i] = y[i] - 32;
+		i++
+	}
+	return (y);
+}
