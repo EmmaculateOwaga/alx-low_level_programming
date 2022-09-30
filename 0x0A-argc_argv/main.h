2526c85b594f8f746735736fa4aadcd64af6809b
EmmaculateOwaga <emmahachieng2015@gmail.com>
2022-09-30 04:30:39 -0700

    main.h header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+
+int _putchar(char c);
+int check_num(char *str);
+
+#endif /* MAIN_H */
